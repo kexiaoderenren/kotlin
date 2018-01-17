442e66711e4c81a37b7a594fe468526ecc595ead
Denis Zharkov <denis.zharkov@jetbrains.com>
2018-01-17 16:28:01 +0300

    Get rid of compile dependency to 'compiler-embeddable' from 'idea'
    :kotlin-compiler-runner has a runtime dependency to :kotlin-compiler-embeddable
    that is imported to :idea as a compile-time dependency (transitively)
    because the default configuration is used there which works the same as "compile"

--- idea/build.gradle.kts
+++ idea/build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     compile(project(":js:js.serializer"))
     compile(project(":compiler:light-classes"))
     compile(project(":compiler:util"))
-    compile(project(":kotlin-compiler-runner"))
+    compile(project(":kotlin-compiler-runner", configuration = "compile"))
     compile(project(":compiler:plugin-api"))
     compile(project(":eval4j"))
     compile(project(":j2k"))
